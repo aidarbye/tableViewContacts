ff0613ce1c36edcd84f012a8ae93048058d4604b
Aidar <audars@mail.ru>
2023-02-13 22:02:30 +0600

    correct uitableview on the second tabbarscreen

--- tableViewControl/Controllers/ShowExactlyTableViewController.swift
+++ tableViewControl/Controllers/ShowExactlyTableViewController.swift
@@ -5,19 +5,34 @@ class ShowExactlyTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func numberOfSections(in tableView: UITableView) -> Int {
         return Users.count
     }
     
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return Users[section].fullName
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        if let header = view as? UITableViewHeaderFooterView {
+            header.textLabel?.shadowColor = .gray
+            }
+    }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "gucci_cell", for: indexPath)
-        cell.detailTextLabel?.numberOfLines = 2
-        cell.textLabel?.text = Users[indexPath.row].fullName
-        cell.textLabel?.shadowColor = .gray
         
-        cell.detailTextLabel?.text = String(Users[indexPath.row].phone) + "\n" + Users[indexPath.row].email
-
+        let person = Users[indexPath.section]
+        
+        switch indexPath.row {
+        case 0:
+            cell.textLabel?.text = String(person.phone)
+            cell.imageView?.image = UIImage(systemName: "phone")
+        default:
+            cell.textLabel?.text = person.email
+            cell.imageView?.image = UIImage(systemName: "tray")
+        }
         return cell
     }
     
